1652ea5563d7cc201281642ae2c3276c2c072995
Mithu0229 <showkat131080@gmail.com>
2023-01-26 11:10:30 +0600

    Add project files.

---
+++ length-of-last-word/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace length_of_last_word
+{
+    public class Program
+    {
+        //leet code
+        static int LastWordLength(string s)
+        {
+            
+            var stringInput=s.TrimEnd().Split(' ');
+            List<string> vs = new List<string>();
+            foreach (var item in stringInput)
+            {
+                vs.Add(item);
+            }
+            string result = vs[vs.Count-1];
+            return result.Length;
+        }
+        static void Main(string[] args)
+        {
+            string s = "Show the message  ";
+            LastWordLength(s);
+            Console.WriteLine(LastWordLength(s));
+        }
+    }
+}
